390b10bb3892944bd092d136ed955d560c71c085
azeem <kr00r4n@gmail.com>
2013-07-21 15:34:07 +0530

    combining primitives. fixes

--- src/Node.js
+++ src/Node.js
@@ -1,49 +1,4 @@
 (function(DataDoo) {
-    /**
-     *  Sphere primitive
-     */
-    function Sphere(radius, color) {
-        this.radius = 10;
-        this.color = color || 0x8888ff;
-        this.center = new DataDoo.AbsolutePosition(0,0,0);
-
-        this.material = new THREE.MeshLambertMaterial({color: this.color});
-        this.geometry = new THREE.SphereGeometry(this.radius);
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.objects = [this.mesh];
-    }
-    Sphere.prototype = {
-        /**
-         * Sets the radius of the sphere
-         */
-        setRadius : function(radius) {
-            this.radius = radius;
-            this.geometry = new THREE.SphereGeometry(this.radius);
-            this.mesh.setGeometry(this.geometry);
-        },
-        getPositions : function() {
-            return [this.center];
-        },
-        onResolve : function() {
-            this.center.applyToVector(this.mesh.position);
-        }
-    };
 
 
-    /**
-     * Node is a visual representation for each datapoint
-     * It contains a set of graphics primitives that reprents
-     * its visual
-     */
-    function Node(data) {
-        this.primitives = [];
-        this.data = data;
-    }
-    Node.prototype.addSphere = function(radius, color) {
-        var sphere = new Sphere(radius, color);
-        this.primitives.push(sphere);
-        return sphere;
-    };
-
-    DataDoo.Node = Node;
 })(window.DataDoo);
--- src/Relation.js
+++ src/Relation.js
@@ -1,27 +1,4 @@
 (function(DataDoo) {
-    /**
-     *  Line primitive
-     */
-    function DashedLine(color, dashSize, gapSize, sourceNode, destNode) {
-        this.dashSize = dashSize || 4;
-        this.gapSize = gapSize || 2;
-        this.color = color || 0x8888ff;
-        this.sourcePosition = sourceNode.position || new THREE.Vector3(0,0,0);
-        this.destinationPosition = destNode.position || new THREE.Vector3(10,10,10);
-
-        this.material = new THREE.MeshLambertMaterial({color: this.color});
-        this.geometry = new THREE.SphereGeometry(this.radius);
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.objects = [this.mesh];
-
-        this.lineGeometry = new THREE.Geometry();
-        var vertArray = this.lineGeometry.vertices;
-        vertArray.push( this.sourcePosition, this.destinationPosition);
-        this.lineGeometry.computeLineDistances();
-        this.lineMaterial = new THREE.LineDashedMaterial( { color: this.color, dashSize: this.dashSize, gapSize: this.gapSize } );
-        this.mesh = new THREE.Line( this.lineGeometry, this.lineMaterial );
-        this.objects = [this.mesh];
-    }
 
     /**
      * Relation is a visual representation of connections between nodes
